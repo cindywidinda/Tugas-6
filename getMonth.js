0bfd3da656a41c430b531dd2eb380c87c7973802
Tribe <cindywidinda@gmail.com>
2022-12-01 11:06:16 +0700

    nomer 2

---
+++ getMonth.js
@@ -0,0 +1,21 @@
+const getMonth = (callback) => {
+    setTimeout(() =>{
+        let error = false
+        let month = ['Januari', 'Februari', 'Maret',
+                    'April', 'Mei', 'Juni', 'Juli',
+                    'Agustus', 'September', 'Oktober',
+                    'November', 'Desember']
+        if (!error){
+            callback(null, month)
+        } else {
+            callback(new Error('Sorry Data Not Found'), [])
+        }
+    }, 4000)
+}
+
+getMonth((gagal,bulan)=>{
+    if(gagal){
+        console.log(gagal.message)
+    }
+    console.log(bulan)
+})
